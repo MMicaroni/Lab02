c5baa5769dd05df05da4bd66edf83fbbc7b3ecce
manuelmicaroni <manuelmicaroni@192.168.1.66>
2018-03-14 15:32:23 +0100

    Versione soluzione punti 1 e 2

--- Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -16,7 +16,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
-	
+	AlienDictionary dictionary = new AlienDictionary();
     @FXML
     private ResourceBundle resources;
     @FXML
@@ -39,12 +39,49 @@ public class AlienController {
     	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
     	
     }
-  
+    
     
     @FXML
     void doTranslate(ActionEvent event) {
     	    	
-    }
+    	String input = txtWord.getText();
+    	String array[] = input.trim().split("\\s+");
+    	
+    	if(array.length==2) {
+    		
+    		String alienW = array[0];
+    		String transl=array[1];
+    	 
+    		if(alienW.matches("[a-zA-Z]+")==false || transl.matches("[a-zA-Z]+")==false) {
+        		txtResult.setText("Hai inserito dei caratteri non validi\n");
+        		return;  
+    		}
+    	    
+    		
+    		//if(dictionary.isIn(alienW)==false)
+    		  dictionary.addWord(alienW, transl);    	
+    	}
+    	if(array.length==1) {
+    		
+    		String alienW=array[0];
+    		if(alienW.matches("[a-zA-Z]+")==false) {
+        		txtResult.setText("Hai inserito dei caratteri non validi\n");
+        	    return;
+    		}
+    		
+    		if(dictionary.isIn(alienW)==true)
+    			txtResult.setText(dictionary.translateWord(alienW));
+    		else
+    			txtResult.setText("La parola non è presente nel database, inserisci la parola nel dizionario specificando la traduzione\n");
+
+    	}
+    	
+    
+    	}
+    	
+    		
+    	
+    	
     
     
     @FXML
